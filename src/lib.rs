ec02be0247166be7f43ddda9a223c1a525d45272
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-27 22:03:20 -0800

    replace .unwrap() with ? and return Result<(i32, String), ParseIntError>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-pub fn parse_money(input: &str) -> (i32, String) {
+use std::num::ParseIntError;
+
+pub fn parse_money(input: &str) -> Result<(i32, String), ParseIntError> {
     let parts: Vec<&str> = input.split_whitespace().collect();
-    let amount = parts[0].parse().unwrap();
+    let amount = parts[0].parse()?;
     let currency = parts[1].to_string();
-    (amount, currency)
+    Ok((amount, currency))
 }
 
 #[cfg(test)]
@@ -10,9 +12,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let (amount, currency) = parse_money("100 dollars");
-        assert_eq!(amount, 100);
-        assert_eq!(currency, "dollars");
+    fn it_parses_money() {
+        if let Ok((amount, currency)) = parse_money("100 dollars"){
+            assert_eq!(amount, 100);
+            assert_eq!(currency, "dollars");
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn it_errors_on_floats() {
+        assert!(parse_money("100.01 dollars").is_err());
     }
 }
